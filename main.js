7788d240df037f8f49dfdcf603613796bc42af2f
timo <tpetmanson@gmail.com>
2023-04-23 01:07:55 +0300

    Added infite scroll, added some content I shouldve done long time ago

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+// Message: thanks for checking out the source code of my homepage.
+// Please enjoy the high quality Javascript you will find here.
+
 // Helper function for creating galleries.
 function CreateLightGalleryElements(container_id, path, from, to) {
   let container = document.getElementById(container_id)
@@ -24,3 +27,27 @@ window.addEventListener('resize', UpdateVhProperty);
 window.addEventListener('load', UpdateVhProperty);
 window.addEventListener('focus', UpdateVhProperty);
 UpdateVhProperty();
+
+// Behold, "infinite scroll"!!
+var allArticles = []
+var numVisibleArticles = 2
+
+function initarticles() {
+  allArticles = document.getElementsByTagName('article');
+  console.log("Loaded " + allArticles.length + " articles.");
+  for (let idx=0 ; idx<Math.min(allArticles.length, numVisibleArticles) ; idx++) {
+    allArticles[idx].classList.add('visiblearticle');
+  }
+}
+
+window.addEventListener('DOMContentLoaded', initarticles);
+window.onscroll = function(ev) {
+  // https://stackoverflow.com/questions/9439725/how-to-detect-if-browser-window-is-scrolled-to-bottom
+  if ((window.innerHeight + window.scrollY) + 1 >= document.body.offsetHeight) {
+    if (numVisibleArticles < allArticles.length) {
+      allArticles[numVisibleArticles].classList.add('visiblearticle');
+      numVisibleArticles+=1;
+      console.log(numVisibleArticles);
+    }
+  }
+};
